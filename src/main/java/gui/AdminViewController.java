888bd2ab570f89c7402ba4dd1bc11d307a6881e7
AtanasLS <atanaslstoyanov.03@gmail.com>
2023-04-26 23:49:22 +0200

    Adding more fxml files and some css

--- src/main/java/gui/AdminViewController.java
+++ src/main/java/gui/AdminViewController.java
@@ -6,37 +6,58 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.Window;
 import main.java.Main;
 
+import java.awt.*;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class AdminViewController implements Initializable {
-
+    @FXML
+    public Button btnLogIns;
     @FXML
     AnchorPane painnnnn;
     @FXML
     private Stage primaryStage;
 
-    public void handleClicks(ActionEvent actionEvent) {
 
-    }
+
+
+
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+
         try {
             FXMLLoader loader = new FXMLLoader();
            // loader.setLocation(Main.class.getResource("/view/LoginPageView.fxml"));;
-            painnnnn.getChildren().setAll((Node) loader.load(getClass().getResource("/view/LoginPageView.fxml")));
-            LoginPageController controller = loader.getController();
+            painnnnn.getChildren().setAll((Node) loader.load(getClass().getResource("/view/DocumentsPage.fxml")));
+            DocumentController controller = loader.getController();
+
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
 
+    }
+
+    public void handleClicks(ActionEvent actionEvent) {
+        if (actionEvent.getSource() == btnLogIns) {
+            try {
+                FXMLLoader loader = new FXMLLoader();
+                // loader.setLocation(Main.class.getResource("/view/LoginPageView.fxml"));;
+                painnnnn.getChildren().setAll((Node) loader.load(getClass().getResource("/view/LoginPageView.fxml")));
+                LoginPageController controller = loader.getController();
+
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 }
---
+++ src/main/java/gui/DocumentController.java
@@ -0,0 +1,38 @@
+package main.java.gui;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.layout.VBox;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class DocumentController implements Initializable {
+
+    @FXML
+    VBox pnItems = null;
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        Node[] nodes = new Node[10];
+        for (int i = 0; i < nodes.length; i++) {
+            try {
+                final int j = i;
+                nodes[i] = FXMLLoader.load(getClass().getResource("/view/Item.fxml"));
+
+                //give the items some effect
+                nodes[i].setOnMouseEntered(event -> {
+                    nodes[j].setStyle("-fx-background-color : #0A0E3F");
+                });
+                nodes[i].setOnMouseExited(event -> {
+                    nodes[j].setStyle("-fx-background-color : #02030A");
+                });
+                pnItems.getChildren().add(nodes[i]);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
